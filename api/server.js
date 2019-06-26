23e75b69308e0453d2d50036a911334c9edec980
anthony <stackchain@gmail.com>
2019-06-26 13:25:55 -0400

    commit - AWS S3 image storage code added

--- api/server.js
+++ api/server.js
@@ -1,5 +1,6 @@
 
 const express = require('express');
+const aws = require('aws-sdk');
 const helmet = require('helmet');
 const cors = require('cors');
 const apiRoute = require('../routes/api/index.js');
@@ -15,6 +16,41 @@ server.get('/', (req, res) => {
     <h1>Working...</h1>
     `);
 });
+
+// CODE BELOW IS FOR UPLOADING TO AWS S3 - SHOULD BE MOVED TO AN API ROUTE EVENTUALLY 
+
+aws.config.region = 'us-east-1';
+
+const S3_BUCKET = "stachowitz-expat-journal"
+
+server.get('/sign-s3', (req, res) => {
+    const s3 = new aws.S3();
+    const fileName = req.query['file-name'];
+    const fileType = req.query['file-type'];
+    const s3Params = {
+        Bucket: S3_BUCKET,
+        Key: fileName,
+        Expires: 60,
+        ContentType: fileType,
+        ACL: 'public-read'
+    };
+
+    s3.getSignedUrl('putObject', s3Params, (err, data) => {
+        if (err) {
+            console.log(err);
+            return res.end();
+        }
+        const returnData = {
+            signedRequest: data,
+            url: `https://${S3_BUCKET}.s3.amazonaws.com/${fileName}`
+        };
+        res.write(JSON.stringify(returnData));
+        res.end();
+    });
+});
+
+// CODE ABOVE IS FOR UPLOADING TO AWS S3 - SHOULD BE MOVED TO AN API ROUTE EVENTUALLY 
+
 
 server.use('/api', apiRoute);
 
